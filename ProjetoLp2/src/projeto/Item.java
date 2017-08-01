210856749c5ac3fefefd02ceb75f7d0aa5ae9eba
javanlacerda <javkaue@gmail.com>
2017-08-01 15:20:05 -0300

    Classes Item Bluray e Series add

--- ProjetoLp2/src/projeto/Item.java
+++ ProjetoLp2/src/projeto/Item.java
@@ -2,4 +2,31 @@ package projeto;
 
 public class Item {
 
+	protected String nome;
+	private int valor;
+	private boolean EstadoDeEmprestimo = false;
+
+	public boolean isEstadoDeEmprestimo() {
+		return EstadoDeEmprestimo;
+	}
+
+	public Item(String nome, int valor, boolean estadoDeEmprestimo) {
+		super();
+		this.nome = nome;
+		this.valor = valor;
+		EstadoDeEmprestimo = estadoDeEmprestimo;
+	}
+
+	public void setEstadoDeEmprestimo(boolean estadoDeEmprestimo) {
+		EstadoDeEmprestimo = estadoDeEmprestimo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
 }
