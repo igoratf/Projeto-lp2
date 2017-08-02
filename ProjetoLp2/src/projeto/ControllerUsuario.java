a63381708fb3eb3cda39730ad7d62e686a328042
Caio Sanches Batista de Lira 116210403 <caiosbl@lcc2-17.lcc.ufcg.edu.br>
2017-08-02 13:13:47 -0300

    Corpo de ControllerUsuario Finalizado

--- ProjetoLp2/src/projeto/ControllerUsuario.java
+++ ProjetoLp2/src/projeto/ControllerUsuario.java
@@ -22,18 +22,18 @@ public class ControllerUsuario {
 	public String getInfoUsuario(String nome, String telefone, String atributo) {
 
 		String chave = nome + telefone;
-		validaParametros(nome, telefone, atributo);
+		validaParametrosGetInfoUsuario(nome, telefone, atributo);
 
 		if (!atributo.equals("Email")) {
 			throw new IllegalArgumentException("Atributo Invalido");
 		} else if (!mapaUsuarios.containsKey(chave)) {
-			throw new IllegalArgumentException("Usuario nao cadastrado");
+			throw new IllegalArgumentException("Usuario invalido");
 		}
 		return mapaUsuarios.get(chave).getEmail();
 
 	}
 
-	private void validaParametros(String nome, String telefone, String atributo) {
+	private void validaParametrosGetInfoUsuario(String nome, String telefone, String atributo) {
 		if (nome == null) {
 			throw new NullPointerException("Nome nulo invalido!");
 		} else if (telefone == null) {
@@ -46,6 +46,55 @@ public class ControllerUsuario {
 			throw new IllegalArgumentException("Telefone vazio invalido!");
 		} else if (atributo.trim().equals("")) {
 			throw new IllegalArgumentException("Atributo vazio invalido!");
+		}
+
+	}
+
+	private void validaParametrosRemoverUsuario(String nome, String telefone, String atributo, String valor) {
+		if (nome == null) {
+			throw new NullPointerException("Nome nulo invalido!");
+		} else if (telefone == null) {
+			throw new NullPointerException("Telefone nulo invalido!");
+		} else if (valor == null) {
+			throw new NullPointerException("Valor nulo invalido!");
+		} else if (nome.trim().equals("")) {
+			throw new IllegalArgumentException("Nome vazio invalido!");
+		} else if (telefone.trim().equals("")) {
+			throw new IllegalArgumentException("Telefone vazio invalido!");
+		} else if (valor.trim().equals("")) {
+			throw new IllegalArgumentException("Valor vazio invalido!");
+		} else if (atributo == null) {
+			throw new NullPointerException("Atributo nulo invalido");
+		}
+
+	}
+
+	public void removerUsuario(String nome, String telefone) {
+		String chave = nome + telefone;
+		if (!mapaUsuarios.containsKey(chave)) {
+			throw new IllegalArgumentException("Usuario invalido");
+		}
+		mapaUsuarios.remove(chave);
+	}
+
+	public String atualizarUsuario(String nome, String telefone, String atributo, String valor) {
+		String chave = nome + telefone;
+		validaParametrosRemoverUsuario(nome, telefone, atributo, valor);
+
+		if (!mapaUsuarios.containsKey(chave)) {
+			throw new IllegalArgumentException("Usuario invalido");
+		}
+
+		switch (atributo) {
+		case "Nome":
+			mapaUsuarios.get(chave).setNome(valor);
+		case "Telefone":
+			mapaUsuarios.get(chave).setNumCelular(valor);
+		case "Email":
+			mapaUsuarios.get(chave).setEmail(valor);
+
+		default:
+			throw new IllegalArgumentException("Atributo invalido");
 		}
 
 	}
