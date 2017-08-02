b76a0c5c9c769d7348d1c536b4893fdb1406a9aa
Caio Sanches Batista de Lira 116210403 <caiosbl@lcc2-22.lcc.ufcg.edu.br>
2017-08-02 11:37:19 -0300

    Adicionado método validaParametros em ControllerUsuario

--- ProjetoLp2/src/projeto/ControllerUsuario.java
+++ ProjetoLp2/src/projeto/ControllerUsuario.java
@@ -1,36 +1,53 @@
 package projeto;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ControllerUsuario {
-	private Set<Usuario> conjuntoUsuarios;
+	private Map<String, Usuario> mapaUsuarios;
 
 	public ControllerUsuario() {
-		conjuntoUsuarios = new HashSet<Usuario>();
+		mapaUsuarios = new HashMap<String, Usuario>();
 	}
 
 	public void cadastrarUsuario(String nome, String telefone, String email) {
 		Usuario usuario = new Usuario(nome, email, telefone);
-		boolean status = conjuntoUsuarios.add(usuario);
-		if (!status) {
+		String chave = nome + telefone;
+		if (mapaUsuarios.containsKey(chave)) {
 			throw new IllegalArgumentException("Usuario ja cadastrado");
 		}
+		mapaUsuarios.put(chave, usuario);
 	}
 
 	public String getInfoUsuario(String nome, String telefone, String atributo) {
-		Usuario usuarioEsperado = new Usuario(nome, "email", telefone);
+
+		String chave = nome + telefone;
+		validaParametros(nome, telefone, atributo);
 
 		if (!atributo.equals("Email")) {
 			throw new IllegalArgumentException("Atributo Invalido");
+		} else if (!mapaUsuarios.containsKey(chave)) {
+			throw new IllegalArgumentException("Usuario nao cadastrado");
 		}
-		for (Usuario usuario : conjuntoUsuarios) {
-			if (usuario.equals(usuarioEsperado)) {
-				return usuario.getEmail();
-			}
+		return mapaUsuarios.get(chave).getEmail();
+
+	}
+
+	private void validaParametros(String nome, String telefone, String atributo) {
+		if (nome == null) {
+			throw new NullPointerException("Nome nulo invalido!");
+		} else if (telefone == null) {
+			throw new NullPointerException("Telefone nulo invalido!");
+		} else if (atributo == null) {
+			throw new NullPointerException("Atributo nulo invalido!");
+		} else if (nome.trim().equals("")) {
+			throw new IllegalArgumentException("Nome vazio invalido!");
+		} else if (telefone.trim().equals("")) {
+			throw new IllegalArgumentException("Telefone vazio invalido!");
+		} else if (atributo.trim().equals("")) {
+			throw new IllegalArgumentException("Atributo vazio invalido!");
 		}
 
-		throw new IllegalArgumentException("Usuário Invalido");
 	}
 
 }
