1c8186adee24c7c29720adc6e73b753c826fe33b
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-08-01 22:51:20 -0300

    Corrigido problema no Build Path, faltava remover as Sources exlcuidas anteriormente no Buid Path

--- ProjetoLp2/src/projeto/ControllerUsuario.java
+++ ProjetoLp2/src/projeto/ControllerUsuario.java
@@ -1,5 +1,34 @@
 package projeto;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ControllerUsuario {
+	private Set<Usuario> conjuntoUsuarios;
+
+	public ControllerUsuario() {
+		conjuntoUsuarios = new HashSet<Usuario>();
+	}
+
+	public void cadastrarUsuario(String nome, String telefone, String email) {
+		Usuario usuario = new Usuario(nome, email, telefone);
+		conjuntoUsuarios.add(usuario);
+	}
+
+	public String getInfoUsuario(String nome, String telefone, String atributo) {
+		Usuario usuarioEsperado = new Usuario(nome, "email", telefone);
+
+		if (atributo.equals("Email")) {
+			for (Usuario usuario : conjuntoUsuarios) {
+				if (usuario.equals(usuarioEsperado)) {
+					return usuario.getEmail();
+				}
+
+			}
+		}
+		return null;
+	}
+	
+	
 
 }
