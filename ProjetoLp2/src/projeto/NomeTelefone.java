78a462b1557a73c75b31558b3a8a8f63820e163c
Caio Sanches Batista de Lira 116210403 <caiosbl@lcc2-38.lcc.ufcg.edu.br>
2017-08-02 15:36:50 -0300

    Corrigido as chaves do mapa de Usuário, criado a classe NomeTelefone que será usada como chave no mapa de usuários,Adicionada a pasta lib com o arquivo da bibliote do EasyAcept.jar, adicionado a pasta diretorio_testes com o teste do caso de uso 1, adicionado a classe Main que roda os testes do Easy Acept

---
+++ ProjetoLp2/src/projeto/NomeTelefone.java
@@ -0,0 +1,43 @@
+package projeto;
+
+public class NomeTelefone {
+	private String nome;
+	private String telefone;
+
+	public NomeTelefone(String nome, String telefone) {
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NomeTelefone other = (NomeTelefone) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+
+}
