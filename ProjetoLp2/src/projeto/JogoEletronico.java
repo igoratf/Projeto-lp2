75d23a1c5ddfb923c8a47773a0eec95baf5802e4
lucasvictor3 <lucas.araujo@ccc.ufcg.edu.br>
2017-08-03 00:05:55 -0300

    Corrigido classe equals de JogosEletronicos

--- ProjetoLp2/src/projeto/JogoEletronico.java
+++ ProjetoLp2/src/projeto/JogoEletronico.java
@@ -3,10 +3,7 @@ package projeto;
 
 
 public class JogoEletronico extends Item {
-	/*
-	 * Lucas, ajeita essa classe de acordo com Item
-	 * lembra de ajeitar o equals também
-	 */
+
 	
 	private Plataforma plataforma;
 	
@@ -16,7 +13,8 @@ public class JogoEletronico extends Item {
 		this.plataforma = plataforma;
 	}
 	
-
+	
+	
 	public Plataforma getPlataforma() {
 		return plataforma;
 	}
@@ -27,7 +25,7 @@ public class JogoEletronico extends Item {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((this.getNome() == null) ? 0 :this.getNome().hashCode());
 		result = prime * result + ((plataforma == null) ? 0 : plataforma.hashCode());
 		return result;
 	}
@@ -41,22 +39,14 @@ public class JogoEletronico extends Item {
 		if (getClass() != obj.getClass())
 			return false;
 		JogoEletronico other = (JogoEletronico) obj;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		if (plataforma == null) {
-			if (other.plataforma != null)
-				return false;
-		} else if (!plataforma.equals(other.plataforma))
+		if (!this.getNome().equals(other.getNome()) && this.plataforma.equals(other.plataforma))
 			return false;
 		return true;
 	}
 	
 	@Override
 	public String toString() {
-		return "JogosEletronicos [nome=" + nome + ", plataforma=" + plataforma + "]";
+		return "JogosEletronicos [nome=" + this.getNome() + ", plataforma=" + plataforma + "]";
 	}
 	
 	
