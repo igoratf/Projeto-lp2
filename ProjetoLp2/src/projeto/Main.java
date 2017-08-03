9f3323d95f4d0b47777bce6ab09280f64fce5ec0
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-08-02 21:09:46 -0300

    adicionada coisas novas

--- ProjetoLp2/src/projeto/Main.java
+++ ProjetoLp2/src/projeto/Main.java
@@ -4,8 +4,15 @@ import easyaccept.EasyAccept;
 
 public class Main {
 	public static void main(String[] args) {
-	    args = new String[] {"projeto.Facade", "diretorio_testes/us1_test.txt"}; //separe cada script de teste por virgula.
-	    EasyAccept.main(args);
+		args = new String[] { "projeto.Facade", "diretorio_testes/us1_test.txt", "diretorio_testes/us2_test.txt",
+				"diretorio_testes/us3_test.txt", "diretorio_testes/us4_test.txt" }; // separe
+																					// cada
+																					// script
+																					// de
+																					// teste
+																					// por
+																					// virgula.
+		EasyAccept.main(args);
 	}
 
 }
