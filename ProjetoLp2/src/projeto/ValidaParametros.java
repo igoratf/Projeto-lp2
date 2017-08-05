6311dc6304bc0758d882bd5747824c05663fcf6c
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-08-05 11:39:19 -0300

    Validadores de parametros removidos de controller usuario e encapsulados em uma nova classee: Valida parametro

---
+++ ProjetoLp2/src/projeto/ValidaParametros.java
@@ -0,0 +1,51 @@
+package projeto;
+
+public class ValidaParametros {
+	public static void validaParametrosGetInfoUsuario(String nome, String telefone, String atributo) {
+		if (nome == null) {
+			throw new NullPointerException("Nome nulo invalido!");
+		} else if (telefone == null) {
+			throw new NullPointerException("Telefone nulo invalido!");
+		} else if (atributo == null) {
+			throw new NullPointerException("Atributo nulo invalido!");
+		} else if (nome.trim().equals("")) {
+			throw new IllegalArgumentException("Nome vazio invalido!");
+		} else if (telefone.trim().equals("")) {
+			throw new IllegalArgumentException("Telefone vazio invalido!");
+		} else if (atributo.trim().equals("")) {
+			throw new IllegalArgumentException("Atributo vazio invalido!");
+		}
+	}
+
+	public static void validaParametrosRemoverUsuario(String nome, String telefone, String atributo, String valor) {
+		if (nome == null) {
+			throw new NullPointerException("Nome nulo invalido!");
+		} else if (telefone == null) {
+			throw new NullPointerException("Telefone nulo invalido!");
+		} else if (valor == null) {
+			throw new NullPointerException("Valor nulo invalido!");
+		} else if (nome.trim().equals("")) {
+			throw new IllegalArgumentException("Nome vazio invalido!");
+		} else if (telefone.trim().equals("")) {
+			throw new IllegalArgumentException("Telefone vazio invalido!");
+		} else if (valor.trim().equals("")) {
+			throw new IllegalArgumentException("Valor vazio invalido!");
+		} else if (atributo == null) {
+			throw new NullPointerException("Atributo nulo invalido");
+		}
+
+	}
+
+	public static void validaDados(String nome, String telefone) {
+		if (nome == null) {
+			throw new NullPointerException("Nome nulo invalido");
+		} else if (telefone == null) {
+			throw new NullPointerException("Telefone nulo invalido");
+		} else if (nome.trim().equals("")) {
+			throw new IllegalArgumentException("Nome vazio invalido");
+		} else if (telefone.trim().equals("")) {
+			throw new IllegalArgumentException("Telefone vazio invalido");
+		}
+	}
+
+}
