fecd64401f8ebd76dfbc09ed2f98de78a583706d
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-08-06 20:43:15 -0300

    Métodos de Check refatorados.

--- ProjetoLp2/src/projeto/ControllerItem.java
+++ ProjetoLp2/src/projeto/ControllerItem.java
@@ -12,7 +12,6 @@ public class ControllerItem {
 		Locale.setDefault(new Locale("en", "US"));
 	}
 
-
 	public void cadastrarEletronico(String nomeItem, double preco, String plataforma) {
 		validaPreco(preco);
 		Item jogoEletronico = new JogoEletronico(nomeItem, preco, plataforma);
@@ -25,24 +24,27 @@ public class ControllerItem {
 		listaItens.add(jogoTabuleiro);
 	}
 
-	public void cadastrarBluRayFilme(String nomeItem, double preco, int duracao, String genero, String classificacao, int anoLancamento) {
+	public void cadastrarBluRayFilme(String nomeItem, double preco, int duracao, String genero, String classificacao,
+			int anoLancamento) {
 		validaPreco(preco);
 		Bluray blurayFilme = new BlurayFilme(nomeItem, preco, duracao, classificacao, genero);
 		listaItens.add(blurayFilme);
 	}
-	
-	public void cadastrarBluRaySerie(String nomeItem, double preco, String descricao, int duracao, String classificacao, String genero, int temporada) {
+
+	public void cadastrarBluRaySerie(String nomeItem, double preco, String descricao, int duracao, String classificacao,
+			String genero, int temporada) {
 		validaPreco(preco);
 		Bluray bluraySerie = new BluraySeries(nomeItem, preco, duracao, descricao, classificacao, genero, temporada);
 		listaItens.add(bluraySerie);
 	}
-	
-	public void cadastrarBlurayShow(String nomeItem, double preco, int duracao,int numFaixas,String nomeArtista,String classificacao) {
+
+	public void cadastrarBlurayShow(String nomeItem, double preco, int duracao, int numFaixas, String nomeArtista,
+			String classificacao) {
 		validaPreco(preco);
 		Bluray blurayShow = new BlurayShow(nomeItem, preco, duracao, numFaixas, nomeArtista, classificacao);
 		listaItens.add(blurayShow);
 	}
-	
+
 	public void adicionarBluray(String serie, int duracao) {
 		BlurayEpisodio blurayEpisodio = new BlurayEpisodio(duracao);
 		for (Item item : listaItens) {
@@ -79,7 +81,7 @@ public class ControllerItem {
 			if (item.getNome().equals(nomeItem))
 				return item;
 		}
-			throw new RuntimeException("Item nao encontrado");
+		throw new RuntimeException("Item nao encontrado");
 	}
 
 	/**
@@ -95,8 +97,8 @@ public class ControllerItem {
 	 */
 	public void atualizarItem(String nomeItem, String atributo, String valor) {
 		Item meuItem = getItem(nomeItem);
-		
-		checaSeItemExiste(nomeItem); //MARCA
+
+		checaSeItemExiste(nomeItem); // MARCA
 		if (atributo.equalsIgnoreCase("preco")) {
 			meuItem.setValor(Float.parseFloat(valor));
 		}
@@ -107,8 +109,8 @@ public class ControllerItem {
 
 	public String getInfoItem(String nomeItem, String atributo) {
 		Item meuItem = getItem(nomeItem);
-		
-		checaSeItemExiste(nomeItem); //MARCA
+
+		checaSeItemExiste(nomeItem); // MARCA
 		switch (atributo) {
 		case "Preco":
 			return String.valueOf(meuItem.getValor());
@@ -119,19 +121,19 @@ public class ControllerItem {
 		}
 
 	}
-	
+
 	public void validaPreco(double preco) {
-		if (preco <=0) {
+		if (preco <= 0) {
 			throw new IllegalArgumentException("Preco invalido");
 		}
 	}
-	
-	public boolean checaSeItemExiste(String nomeItem){
+
+	private void checaSeItemExiste(String nomeItem) {
 		for (Item item : listaItens) {
 			if (item.getNome().equals(nomeItem))
-				return true;
-	}
+				return;
+		}
 		throw new IllegalArgumentException("Item nao encontrado");
-  }
-	
+	}
+
 }
