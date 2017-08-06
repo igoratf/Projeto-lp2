508767e00d0cba5ab7fb868bcbd82250daa5ad3a
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-08-06 15:46:06 -0300

    Assinatura de Cadastar BluRayShows alterada

--- ProjetoLp2/src/projeto/BlurayShow.java
+++ ProjetoLp2/src/projeto/BlurayShow.java
@@ -4,9 +4,9 @@ public class BlurayShow extends Bluray {
 
 	private String nomeArtista;
 	private int numFaixas;
-
-	public BlurayShow(String nome, double valor, int duracao, String classificacao,
-			String nomeArtista, int numFaixas) {
+	
+	public BlurayShow(String nome, double valor, int duracao,int numFaixas, 
+			String nomeArtista, String classificacao) {
 		super(nome, valor, duracao, classificacao);
 		this.nomeArtista = nomeArtista;
 		this.numFaixas = numFaixas;
