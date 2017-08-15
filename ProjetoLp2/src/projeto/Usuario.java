45c52a6224378ab5bb00a3498c8ea29cd2e3acf4
caiosbl <caio.lira@ccc.ufcg.edu.br>
2017-08-14 21:52:47 -0300

    Adicionado método de listar Emprestimos emprestados de Usuários

--- ProjetoLp2/src/projeto/Usuario.java
+++ ProjetoLp2/src/projeto/Usuario.java
@@ -1,5 +1,6 @@
 package projeto;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -342,17 +343,20 @@ public class Usuario {
 	}
 
 	/**
-	 * Metodo para retornar a lista de itens que o usuario possui.
-	 * @return, lista de itens do usuario.
+	 * Metodo para retornar a lista de itens que o usuario possui. @return,
+	 * lista de itens do usuario.
 	 */
 	public ArrayList<Item> getListaItens() {
 		return new ArrayList<Item>(listaItens);
 	}
 
 	/**
-	 * Metodo para retornar um item baseado em seu nome pertencente a esse usuario.
-	 * @param nomeItem, String passado por parametro.
-	 * @return, retorna o item desejado.
+	 * Metodo para retornar um item baseado em seu nome pertencente a esse
+	 * usuario.
+	 * 
+	 * @param nomeItem,
+	 *            String passado por parametro. @return, retorna o item
+	 *            desejado.
 	 */
 	public Item getItem(String nomeItem) {
 		for (Item item : listaItens) {
@@ -364,13 +368,19 @@ public class Usuario {
 
 	/**
 	 * Metodo para retornar um emprestimo cadastrado nesse usuario.
-	 * @param dono, Objeto passado por parametro.
-	 * @param requerente, Objeto passado por parametro.
-	 * @param item, Objeto passado por parametro.
-	 * @param dataEmprestimo, String passado por parametro.
-	 * @return, Emprestimo encontrado no usuario.
+	 * 
+	 * @param dono,
+	 *            Objeto passado por parametro.
+	 * @param requerente,
+	 *            Objeto passado por parametro.
+	 * @param item,
+	 *            Objeto passado por parametro.
+	 * @param dataEmprestimo,
+	 *            String passado por parametro. @return, Emprestimo encontrado
+	 *            no usuario.
+	 * @throws ParseException 
 	 */
-	public Emprestimo getEmprestimo(Usuario dono, Usuario requerente, Item item, String dataEmprestimo) {
+	public Emprestimo getEmprestimo(Usuario dono, Usuario requerente, Item item, String dataEmprestimo) throws ParseException {
 		Emprestimo emprestimoParametro = new Emprestimo(dono, requerente, item, dataEmprestimo, 0);
 		for (Emprestimo emprestimo : emprestimos) {
 			if (emprestimo.equals(emprestimoParametro))
@@ -390,7 +400,6 @@ public class Usuario {
 		}
 	}
 
-
 	/**
 	 * Verifica se o usuário contém algum empréstimo
 	 * 
@@ -399,6 +408,16 @@ public class Usuario {
 
 	public boolean contemEmprestimo() {
 		return emprestimos.size() > 0;
+	}
+
+	public ArrayList<Emprestimo> getEmprestimosEmprestados() {
+		ArrayList<Emprestimo> emprestimosTemp = new ArrayList<>();
+		for (Emprestimo emprestimo : emprestimos) {
+			if (emprestimo.getDono().getNome().equals(this.nome) && emprestimo.getDono().getNumCelular() == this.numCelular) {
+				emprestimosTemp.add(emprestimo);
+			}
+		}
+		return emprestimosTemp;
 	}
 
 	/**
